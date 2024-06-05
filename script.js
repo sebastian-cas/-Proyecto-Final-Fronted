394daccaf882be5259dedb4967cb6e952db1b8ca
sebastian castillo <sebastiancastillo305@gmail.com>
2024-06-05 18:07:35 -0500

    mostrar lista de tareas

---
+++ script.js
@@ -0,0 +1,137 @@
+const tareas = [
+    {
+        "_id": "1",
+        "titulo": "caminar",
+        "descripcion": "salir a caminar en las mañanas",
+        "estado": "activa",
+        "responsable": "sebas"
+    },
+    {
+        "_id": "2",
+        "titulo": "ir al gimnasio",
+        "descripcion": "salir al gimnasio en las tardes luego del trabajo",
+        "estado": "activa",
+        "responsable": "sebas"
+    },
+    {
+        "_id": "3",
+        "titulo": "Pasear al perror",
+        "descripcion": "salir a caminar en las mañanas con el perro",
+        "estado": "activa",
+        "responsable": "sebas"
+    },
+    {
+        "_id": "4",
+        "titulo": "limpiar la cocina",
+        "descripcion": "limpiar la cocina despues de cocinar",
+        "estado": "activa",
+        "responsable": "sebas"
+    }
+]
+
+const crearTarea = async (tarea) => {
+    // enviar consulta a la API para crear una tarea
+    alert('tarea creada')
+}
+
+const obtenerTareas = async () => {
+    // enviar consulta a la API para obtener todas las tareas
+
+    return tareas
+}
+
+const verTarea = async (id) => {
+    // enviar consulta a la API para obtener la tarea con el id
+    alert('tera obtenida')
+    return {
+        "_id": "4",
+        "titulo": "caminata en las mañanas",
+        "descripcion": "salir a caminar en las mañanas",
+        "estado": "activa",
+        "responsable": "sebas"
+    }
+}
+
+const editarTarea = async (id) => {
+    // enviar consulta a la API para obtener la tarea con el id
+    alert('tarea editada')
+}
+
+
+const eliminarTarea = async (id) => {
+    // enviar consulta a la API para eliminar la tarea con el id
+    alert('tarea eliminada')
+}
+
+// -----------------------  Renderizar tareas en el HTML -----------------------
+const listaTareas = document.getElementById('lista-tareas')
+const renderTareas = async () => {
+    
+    const listaTareasObtenidas = await obtenerTareas()
+
+    // blucle para recorrer cada tarea
+    listaTareasObtenidas.forEach((tarea) => {
+        //console.log(tarea)
+
+        const listItem = document.createElement('li')
+        const article = document.createElement('article')
+        const datos = document.createElement('div')
+        datos.classList.add('tarea')
+
+        const titulo = document.createElement('h4')
+        const estado = document.createElement('p')
+        const responsable = document.createElement('p')
+
+        //titulo.innerText = `Titulo: ${tarea.titulo}`
+        titulo.innerText = 'Titulo: ' + tarea.titulo
+        estado.innerText = `Estado: ${tarea.estado}`
+        responsable.innerText = `Responsable ${tarea.responsable}`
+
+        datos.appendChild(titulo)
+        datos.appendChild(estado)
+        datos.appendChild(responsable)
+
+        article.appendChild(datos)
+        listItem.appendChild(article)
+
+        listaTareas.appendChild(listItem)
+
+
+
+    })
+
+}
+
+// -----------------------  Abrir y Cerra ventana crear tarea -----------------------
+const wrapperFormCrear = document.getElementById('wrapper-form-crear')
+
+const buttonAbrirFormCrear = document.getElementById('abrir-form-crear')
+buttonAbrirFormCrear.addEventListener('click', () => {
+    wrapperFormCrear.style.display = 'grid'
+})
+
+const buttonCerrarFormCrear = document.getElementById('cerrar-form-crear')
+buttonCerrarFormCrear.addEventListener('click', () => {
+    wrapperFormCrear.style.display = 'none'
+})
+
+// -----------------------  Abrir ventana editar tarea -----------------------
+const buttonCerrarFormEditar = document.getElementById('cerrar-form-editar')
+buttonCerrarFormEditar.addEventListener('click', () => {
+    const editarTarea = document.getElementById('wrapper-form-editar')
+    editarTarea.style.display = 'none'
+})
+
+// -----------------------  Crear tarea -----------------------
+const formCrearTarea = document.getElementById('form-crear-tarea')
+formCrearTarea.addEventListener('submit', async (e) => {
+
+})
+
+// -----------------------  Filtrar tareas por estado -----------------------
+let estado = ''
+const selectEstado = document.getElementById('select-estado')
+selectEstado.addEventListener('change', (e) => {
+})
+
+window.addEventListener('load', renderTareas)
